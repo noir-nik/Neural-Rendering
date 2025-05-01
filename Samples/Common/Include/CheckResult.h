30bae1946b4b219ba080dde09d3b79159212ff6b
nm <niki8fw@gmail.com>
2025-05-01 09:58:29 +0300

    Move headers to common

---
+++ Samples/Common/Include/CheckResult.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define _CHECK_VULKAN_RESULT2(func, line) \
+	{ \
+		::vk::Result local_result_##line = ::vk::Result(func); \
+		if (local_result_##line != ::vk::Result::eSuccess) { \
+			::std::printf("Vulkan error: %s " #func " in " __FILE__ ":" #line, ::vk::to_string(local_result_##line).c_str()); \
+			::std::exit(1); \
+		} \
+	}
+
+#define _CHECK_VULKAN_RESULT(func, line) _CHECK_VULKAN_RESULT2(func, line)
+#define CHECK_VULKAN_RESULT(func) _CHECK_VULKAN_RESULT(func, __LINE__)
